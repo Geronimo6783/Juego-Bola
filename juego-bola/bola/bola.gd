44aefc0b59776499aa9635bd91651d711ab36e1d
Lois Domínguez Domínguez <Geronimo6783@gmail.com>
2025-03-20 21:58:05 +0100

    Código de la bola corregido usando el parámetro delta.

--- juego-bola/bola/bola.gd
+++ juego-bola/bola/bola.gd
@@ -1,10 +1,13 @@
 extends RigidBody2D
 
+## Velocidad de desplazamiento de la bola.
+var velocidad_desplacamiento = 200
+
 ## Función que se procesa por cada fotograma.
 func _process(delta: float):
 	if Input.is_action_pressed("izquierda"):
-		move_and_collide(Vector2(-3,0))
+		move_and_collide(Vector2(- velocidad_desplacamiento * delta,0))
 	if Input.is_action_pressed("derecha"):
-		move_and_collide(Vector2(3,0))
+		move_and_collide(Vector2(velocidad_desplacamiento * delta,0))
 	if Input.is_action_pressed("arriba"):
-		move_and_collide(Vector2(0,-3))
+		move_and_collide(Vector2(0,- velocidad_desplacamiento * delta))
